4ed17d63740d8d93a59358e961ae1d3d894966ca
Capsnow467patrick@pateco.ch <ton-email@example.com>
2025-12-11 21:19:42 +0100

    Fix: Update main.py and Procfile for Railway deployment

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import uvicorn
 from app import create_app
+import uvicorn
 from settings.local import PORT
+import os
 
 app = create_app()
 
-if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=PORT) 
-    
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", PORT))
+    uvicorn.run(app, host="0.0.0.0", port=port)
